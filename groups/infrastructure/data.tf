cf09a46c97b667a67b64821aa4827793115fe5c2
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-08-07 13:44:53 +0100

    Removed remote_state lookups, add data lookups to obtain the required data

--- groups/infrastructure/data.tf
+++ groups/infrastructure/data.tf
@@ -1,21 +1,21 @@
-data "terraform_remote_state" "networks" {
-  backend = "s3"
-  config = {
-    bucket = var.remote_state_bucket
-    key    = "${var.state_prefix}/${var.deploy_to}/${var.deploy_to}.tfstate"
-    region = var.aws_region
-  }
-}
-
-data "terraform_remote_state" "services-stack-configs" {
-  backend = "s3"
-  config = {
-    bucket = var.aws_bucket # aws-common-infrastructure-terraform repo uses the same remote state bucket
-    key    = "aws-common-infrastructure-terraform/common-${var.aws_region}/services-stack-configs.tfstate"
-    region = var.aws_region
-  }
-}
-
 data "vault_generic_secret" "secrets" {
   path = "applications/${var.aws_profile}/${var.environment}/${local.stack_fullname}"
+}
+
+data "aws_kms_key" "stack_configs" {
+  key_id = "alias/${var.aws_profile}/${local.kms_key_alias}"
+}
+
+data "aws_subnets" "application" {
+  filter {
+    name   = "tag:Name"
+    values = [local.application_subnet_pattern]
+  }
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.vpc_name]
+  }
 }
