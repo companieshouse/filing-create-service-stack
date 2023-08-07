e6c94eef69678f84f484fc0d155c435f16e8fb7e
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-08-07 13:44:26 +0100

    Cleanup unused locals, add locals to hold secret data

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -3,6 +3,17 @@ locals {
   stack_fullname = "${local.stack_name}-stack"
   name_prefix    = "${local.stack_name}-${var.environment}"
 
-  vpc_id            = data.terraform_remote_state.networks.outputs.vpc_id
-  application_ids   = data.terraform_remote_state.networks.outputs.application_ids
+  stack_secrets              = jsondecode(data.vault_generic_secret.secrets.data_json)
+
+  application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
+  application_subnet_ids     = join(",", data.aws_subnets.application.ids)
+  kms_key_alias              = local.stack_secrets["kms_key_alias"]
+  vpc_name                   = local.stack_secrets["vpc_name"]
+
+  parameter_store_secrets    = {
+    "web-oauth2-client-id"     = local.stack_secrets["web-oauth2-client-id"],
+    "web-oauth2-client-secret" = local.stack_secrets["web-oauth2-client-secret"],
+    "web-oauth2-cookie-secret" = local.stack_secrets["web-oauth2-cookie-secret"],
+    "web-oauth2-request-key"   = local.stack_secrets["web-oauth2-request-key"]
+  }
 }
